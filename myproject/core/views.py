be1d8d0dff267bf23118e48e758fabee68346263
rg3915 <rg3915@yahoo.com.br>
2016-12-22 00:14:29 -0200

    Using try

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -8,8 +8,14 @@ def counter_word(request):
     w is the parameter to word.
     Type: localhost:8000/api/?url=python.org&w=Python
     '''
-    url = request.GET['url']
-    word = request.GET['w']
+    try:
+        url = request.GET['url']
+    except KeyError:
+        url = ''
+    try:
+        word = request.GET['w']
+    except KeyError:
+        word = ''
     if url is not None and word is not None:
         res = find_word_occurrences(url, word)
     return HttpResponse(JsonResponse(res))
